ccea7809a058f18bdb50c5135381bb458a8464d7
jvillacampa <jaime.villacampa@hotmail.com>
2019-01-18 22:13:51 +0000

    formatting function and adding functional groups

--- Scripts R/Diversity analysis.R
+++ Scripts R/Diversity analysis.R
@@ -8,35 +8,62 @@ source("Scripts R/Functions and packages.R")
 
 amph_data <- readRDS("Datos/amph_data_basefile.rds") # basefile
 
+##########################################################.
 # Preparing files needed for iNEXT diversity calculations
-abun_band <- amph_data %>% mutate(site = paste0("Band", site)) %>% 
-  group_by(site, species) %>% count() %>% spread (site, n) %>% 
-  mutate_all(funs(replace(., is.na(.), 0))) %>% ungroup() %>% 
-  select(-species) %>% as.list()
+# Abundance by band: total and by functional category
+abun_band <- amph_data %>% mutate(group = paste0("B", site)) %>% make_inext_file()
 
-# Don't need it
-# Creating file with matrix for each band with a column per transect(sampling unit)
-# abun_band_transect <- amph_data %>% mutate(site = paste0("Band", site)) %>% 
-#   group_by(transect, site, species) %>% count() %>% ungroup()
-# 
-# abun_sampling_unit <- list(band500 = make_band_files("Band500"), band700 = make_band_files("Band700"),
-#              band900 = make_band_files("Band900"), band1100 = make_band_files("Band1100"))
+abun_band_repro <- amph_data %>% mutate(group = paste0("B", site, reproduction)) %>% 
+  make_inext_file()
 
-abun_transect <- amph_data %>% 
-  group_by(transect, species) %>% count() %>% spread (transect, n) %>% 
-  mutate_all(funs(replace(., is.na(.), 0))) %>% ungroup() %>% 
-  select(-species) %>% as.list()
+abun_band_weight <- amph_data %>% mutate(group = paste0("B", site, weight_group)) %>% 
+  make_inext_file()
+
+abun_band_habitat <- amph_data %>% mutate(group = paste0("B", site, habitat)) %>% 
+  make_inext_file()
+
+##########################################################.
+# Abundance by transect: total and by functional category
+abun_transect <- amph_data %>% mutate(group = transect) %>% make_inext_file()
+
+abun_trans_repro <- amph_data %>% mutate(group = paste0("B", site, "_", transect, reproduction)) %>% 
+  make_inext_file()
+
+abun_trans_weight <- amph_data %>% mutate(group = paste0("B", site, "_", transect, weight_group)) %>% 
+  make_inext_file()
+
+abun_trans_habitat <- amph_data %>% mutate(group = paste0("B", site, "_", transect, habitat)) %>% 
+  make_inext_file()
 
 ##########################################################.
 ## Diversity analisys ----
 ##########################################################.
 # running diversity analysis. Endpoint double minimum sample size (Colwell et al.)
-band_div_results <- iNEXT(abun_band, q=c(0,1,2), datatype="abundance",
+##########################################################.
+# Diversity by band: total and by functional category
+band_div_total <- iNEXT(abun_band, q=c(0,1,2), datatype="abundance",
                           endpoint = 132)
 
-transect_div_results <- iNEXT(abun_transect, q=c(0,1,2), datatype="abundance")
+band_div_repro <- iNEXT(abun_band_repro, q=c(0,1,2), datatype="abundance",
+                          endpoint = 132)
 
+band_div_repro <- iNEXT(abun_band_repro, q=c(0,1,2), datatype="abundance",
+                        endpoint = 132)
+
+band_div_weight <- iNEXT(abun_band_weight, q=c(0,1,2), datatype="abundance",
+                        endpoint = 132)
+
+band_div_habitat <- iNEXT(abun_band_habitat, q=c(0,1,2), datatype="abundance",
+                        endpoint = 132)
+
+##########################################################.
+# Diversity by transect: total and by functional category
+transect_div_total <- iNEXT(abun_transect, q=c(0,1,2), datatype="abundance")
+transect_div_repro <- iNEXT(abun_transect_repro, q=c(0,1,2), datatype="abundance")
+transect_div_weight <- iNEXT(abun_transect_weight, q=c(0,1,2), datatype="abundance")
+transect_div_habitat <- iNEXT(abun_transect_habitat, q=c(0,1,2), datatype="abundance")
 
 ggiNEXT(band_div_results, type=1, facet.var="order", color.var="site")
 ggiNEXT(transect_div_results, type=1, facet.var="order", color.var="site")
 
+## END
