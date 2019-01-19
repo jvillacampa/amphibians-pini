0a6d684ac39248a19fea9db658cf87bf711c2c2e
jvillacampa <jaime.villacampa@hotmail.com>
2019-01-19 19:18:17 +0000

    filepaths

--- Scripts R/Starting.R
+++ Scripts R/Starting.R
@@ -6,7 +6,7 @@ source("Scripts R/Functions and packages.R")
 ## Basefile data ----
 ##########################################################.
 #Creating basefile from raw data
-amph_data <- read_excel("Datos/Raw data/Amphibian_survey_data.xlsx", 
+amph_data <- read_excel("Datos/raw_data/Amphibian_survey_data.xlsx", 
                    sheet = "Data", range = "A1:R575") %>% 
   setNames(tolower(names(.))) %>% #variables to lower case
   # selecting only amphibians in ves, exlcuding audio records
@@ -17,22 +17,22 @@ amph_data <- read_excel("Datos/Raw data/Amphibian_survey_data.xlsx",
          transect = tolower(transect)) # some in capital letters
   
 #bringing functional group information
-funct_lookup <- read_csv2("Datos/functional_groups_lookup.csv")
+funct_lookup <- read_csv2("Datos/raw_data/functional_groups_lookup.csv")
 
 #merging with main dataset  
 amph_data <- left_join(amph_data, funct_lookup, by = c("species")) 
 
-saveRDS(amph_data, "Datos/amph_data_basefile.rds")
+saveRDS(amph_data, "Datos/prepared_data/amph_data_basefile.rds")
 
 ##########################################################.
 ## Vegetation mapping data ----
 ##########################################################.
-veg_mapping <- read_excel("Datos/Raw data/Vegmap summary data.xlsx", 
+veg_mapping <- read_excel("Datos/raw_data/Vegmap summary data.xlsx", 
                         sheet = "Varamb.noest", range = "A1:L41") %>% 
   setNames(tolower(names(.))) %>% #variables to lower case
   mutate_if(is.numeric, funs(scale(.))) #scaling variables
 
-saveRDS(veg_mapping, "Datos/vegmap_basefile.rds")
+saveRDS(veg_mapping, "Datos/prepared_data/vegmap_basefile.rds")
 
 
 # END
