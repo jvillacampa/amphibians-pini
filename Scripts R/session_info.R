7c347e30abf6122983dcc7aa444a57f8e0a5ec75
jvillacampa <jaime.villacampa@hotmail.com>
2019-03-10 13:34:25 +0000

    session info

---
+++ Scripts R/session_info.R
@@ -0,0 +1,38 @@
+# R version 3.5.2 (2018-12-20)
+# Platform: x86_64-w64-mingw32/x64 (64-bit)
+# Running under: Windows 7 x64 (build 7601) Service Pack 1
+# 
+# Matrix products: default
+# 
+# locale:
+#   [1] LC_COLLATE=Spanish_Spain.1252  LC_CTYPE=Spanish_Spain.1252    LC_MONETARY=Spanish_Spain.1252
+# [4] LC_NUMERIC=C                   LC_TIME=Spanish_Spain.1252    
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] adespatial_0.3-4 ggpubr_0.2       magrittr_1.5     vegan_2.5-3      permute_0.9-4    tibble_2.0.1    
+# [7] Hmisc_4.1-1      Formula_1.2-3    survival_2.43-3  lattice_0.20-38  ggplot2_3.1.0    tidyr_0.8.2     
+# [13] iNEXT_2.0.18     readr_1.3.1      readxl_1.2.0     dplyr_0.8.0.1   
+# 
+# loaded via a namespace (and not attached):
+#   [1] nlme_3.1-137        sf_0.7-3            gmodels_2.18.1      RColorBrewer_1.1-2  progress_1.2.0     
+# [6] httr_1.4.0          tools_3.5.2         backports_1.1.3     R6_2.3.0            rpart_4.1-13       
+# [11] KernSmooth_2.23-15  spData_0.3.0        DBI_1.0.0           lazyeval_0.2.1      mgcv_1.8-26        
+# [16] colorspace_1.3-2    ade4_1.7-13         nnet_7.3-12         withr_2.1.2         sp_1.3-1           
+# [21] tidyselect_0.2.5    gridExtra_2.3       prettyunits_1.0.2   rematch_1.0.1       compiler_3.5.2     
+# [26] adephylo_1.1-11     htmlTable_1.13.1    expm_0.999-3        xml2_1.2.0          scales_1.0.0       
+# [31] checkmate_1.9.1     classInt_0.3-1      stringr_1.3.1       digest_0.6.18       foreign_0.8-71     
+# [36] base64enc_0.1-3     pkgconfig_2.0.2     htmltools_0.3.6     htmlwidgets_1.3     rlang_0.3.1        
+# [41] rstudioapi_0.9.0    shiny_1.2.0         gtools_3.8.1        acepack_1.4.1       spdep_1.0-2        
+# [46] Matrix_1.2-15       Rcpp_1.0.0          munsell_0.5.0       ape_5.2             stringi_1.2.4      
+# [51] yaml_2.2.0          MASS_7.3-51.1       plyr_1.8.4          adegraphics_1.0-15  grid_3.5.2         
+# [56] gdata_2.18.0        parallel_3.5.2      promises_1.0.1      crayon_1.3.4        deldir_0.1-16      
+# [61] adegenet_2.1.1      rncl_0.8.3          splines_3.5.2       hms_0.4.2           knitr_1.21         
+# [66] pillar_1.3.1        igraph_1.2.4        uuid_0.1-2          boot_1.3-20         seqinr_3.4-5       
+# [71] reshape2_1.4.3      LearnBayes_2.15.1   XML_3.98-1.17       glue_1.3.0          RNeXML_2.3.0       
+# [76] latticeExtra_0.6-28 data.table_1.12.0   httpuv_1.4.5.1      cellranger_1.1.0    gtable_0.2.0       
+# [81] purrr_0.2.5         assertthat_0.2.0    xfun_0.4            mime_0.6            phylobase_0.8.6    
+# [86] xtable_1.8-3        e1071_1.7-0.1       coda_0.19-2         later_0.7.5         class_7.3-14       
+# [91] units_0.6-2         cluster_2.0.7-1    
